f7dc4f83d680f0422d5aad81803b669bc0e60b15
Simon Milord <simon.milord@gmail.com>
2023-06-11 02:08:57 -0400

    added setup for chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.scss';
+import HomePage from './pages/HomePage/HomePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import './styles/_variables.scss';
 
-export default class App extends Component {
-
-  render() {
+export default function App() {
+  const theme: Object = extendTheme({
+    colors: {
+      primary: '#333333',
+      secondary: '#FDB813',
+    },
+  })
     return (
       <>
-        <Router>
-          <div className='App'>
-
-          </div>
-        </Router>
+        <ChakraProvider theme={theme}>
+          <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="*" element={<NotFoundPage />}/>
+          </Routes>
+        </ChakraProvider>
       </>
     )
-  }
 }
