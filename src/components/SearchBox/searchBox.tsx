081f62fc33f3e95d3b28cfb0bb47cf39dcccfbd4
Simon Milord <simon.milord@gmail.com>
2023-12-10 16:51:20 -0500

    added some improvements and logic to searchBox component

--- src/components/SearchBox/searchBox.tsx
+++ src/components/SearchBox/searchBox.tsx
@@ -1,7 +1,8 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Box,
+  Text,
   Input,
   InputGroup,
   InputLeftElement,
@@ -18,66 +19,96 @@ interface SearchBoxProps {
 const SearchBox: React.FC<SearchBoxProps> = () => {
   const [query, setQuery] = useState<string>("");
   const [suggestions, setSuggestions] = useState<string[]>([]);
-
-  // TODO: Write validation function / maybe create utils file?
-  const validateSearch = (value: string) => {};
-
-  useEffect(() => {
-    console.log('SUGGESTIONS: ' + suggestions);
-  }, [suggestions]);
+  const [isSuggestionsOpen, setIsSuggestionsOpen] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   // Function to handle the search query change
-  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const userQuery = e.target.value;
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const userQuery = event.target.value;
     setQuery(userQuery);
+
     const mockSuggestions: Array<string> = [
-      "Harry Potter and the Chamber of secrets",
+      "The old man and the sea",
       "The intelligent investor",
       "The Raven",
+      "The signal and the noise",
+      "The Lord of the Rings",
     ];
-    // Will add fetch from API here later -- TODO --
-    setSuggestions(
-      mockSuggestions.filter((suggestion) => {
-        suggestion.toLowerCase().includes(userQuery.toLowerCase());
-      })
+
+    // Will add fetch from API HERE later -- TODO --
+
+    const filteredSuggestions = mockSuggestions.filter((suggestion: string) =>
+      suggestion.toLowerCase().includes(userQuery.toLowerCase())
     );
+
+    setSuggestions(filteredSuggestions);
+    setIsSuggestionsOpen(filteredSuggestions.length > 0);
   };
 
   // Function to handle the selection from dropdown
   const handleSelectSuggestion = (selectedSuggestion: string) => {
     setQuery(selectedSuggestion);
+    setSuggestions([]);
+    setIsSuggestionsOpen(false);
+
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
   };
 
   // Function to handle the submit / trigger search
-  const handleSubmit = (query: string) => {};
+  const handleSubmit = () => {
+    console.log("Submitted query: " + query);
+    // TODO with the API
+  };
 
   return (
-      <Box>
-      {/* <InputLeftElement>
-        <SearchIcon />
-      </InputLeftElement> */}
-      <Input
-        value={query}
-        onChange={handleQueryChange}
-        placeholder="Search..."
-        size="md"
-        width="xl"
-        />
-      {suggestions.length > 0 && (
-        <Menu>
-          <MenuButton />
-          <MenuList>
-            {suggestions.map((suggestion: string, index: number) => (
-              <MenuItem
-              key={index}
-              onClick={() => handleSelectSuggestion(suggestion)}
-              >
-                {suggestion}
-              </MenuItem>
-            ))}
-          </MenuList>
-        </Menu>
-      )}
+    <Box>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <InputGroup>
+          <InputLeftElement>
+            <SearchIcon />
+          </InputLeftElement>
+          <Input
+            value={query}
+            onChange={handleQueryChange}
+            placeholder="Search..."
+            size="md"
+            width="xl"
+            ref={inputRef}
+          />
+          {suggestions.length > 0 && (
+            <Menu
+              isOpen={isSuggestionsOpen}
+              closeOnSelect={true}
+              closeOnBlur={true}
+            >
+              <MenuButton
+                as={Box}
+                cursor="pointer"
+                position="absolute"
+                left="0"
+                top="100%"
+              ></MenuButton>
+              <MenuList>
+                {suggestions.map((suggestion: string, index: number) => (
+                  <MenuItem
+                    key={index}
+                    onClick={() => handleSelectSuggestion(suggestion)}
+                  >
+                    <Text>{suggestion}</Text>
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          )}
+        </InputGroup>
+      </form>
     </Box>
   );
 };
