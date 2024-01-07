2625dc26fe21070ef56bc8d8b4ad4f9935857532
Simon Milord <simon.milord@gmail.com>
2024-01-07 17:51:10 -0500

    added improvements on booklist and booklistitem components

--- src/components/BookList/booklist.tsx
+++ src/components/BookList/booklist.tsx
@@ -1,22 +1,23 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import BookListItem from "../BookListItem/bookListItem";
+import { Book } from "src/types/book";
 
 const imagePlaceholder =
   "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg";
 
 interface BookListProps {
-  bookArray: any[];
+  booksArray: Book[];
 }
-const BookList = ({ bookArray }: BookListProps): JSX.Element => {
+const BookList = ({ booksArray }: BookListProps): JSX.Element => {
+
   return (
-    <Grid templateColumns="repeat(5, 1fr)" gap={7}>
-      {bookArray &&
-        bookArray.map((book: any) => {
+    <Grid templateColumns="repeat(9, 1fr)" gap={3} my={6}>
+      {booksArray &&
+        booksArray.map((book: any) => {
           return (
-            <GridItem key={book.id}>
+            <GridItem key={book.book_id}>
               <BookListItem
-                title={book.title}
-                author={book.author}
+                id={book.book_id}
                 image={book.image || imagePlaceholder}
               />
             </GridItem>
--- src/components/BookListItem/bookListItem.tsx
+++ src/components/BookListItem/bookListItem.tsx
@@ -1,26 +1,22 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Image, Link } from "@chakra-ui/react";
+import './bookListItem.scss';
 
-interface BookListItemProps {
-  title: string,
-  author?: string,
+export interface BookListItemProps {
+  id: string,
   image?: string,
 }
-// const BookListItem: React.FC<BookListItemProps>  = ({ image }): JSX.Element => {
 
-//   return (
-//     <Box>
-//       <Image src={image} maxH={200}/>
-//     </Box>
-//   );
-// };
-
-const BookListItem: React.FC<BookListItemProps>  = ({ title, image }): JSX.Element => {
+const BookListItem: React.FC<BookListItemProps>  = (props): JSX.Element => {
+  const {id, image } = props;
+  const bookInfoLinkPath = `bookinfo/${id}`;
 
   return (
-    <Box>
-      <p>{title}</p>
-      <Image src={image} maxH={200}/>
-    </Box>
+    <>
+      <Link href={bookInfoLinkPath} className='bookListItem__container'>
+        <Image src={image} fit='contain' maxH={200} h='100%' borderRadius='8px'/>
+      </Link>
+    </>
+
   );
 };
 
