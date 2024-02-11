a2b26709684637d5b1bab9857e0480e776b799b6
Simon Milord <simon.milord@gmail.com>
2024-02-10 22:50:27 -0500

    fixed reading card styling issue

--- src/components/ReadingCard/readingCard.tsx
+++ src/components/ReadingCard/readingCard.tsx
@@ -4,37 +4,41 @@ import {
   Grid,
   GridItem,
   Image,
-  Spacer,
   Progress,
   Link as ChakraLink,
+  Heading,
 } from "@chakra-ui/react";
-import './readingCard.scss';
+import "./readingCard.scss";
 import { Book } from "src/types/types";
-import { Link as ReactRouterLink } from 'react-router-dom';
+import { Link as ReactRouterLink } from "react-router-dom";
 
 export interface ReadingCardProps {
-  book: Book,
+  book: Book;
 }
 
 const ReadingCard: React.FC<ReadingCardProps> = ({ book }): JSX.Element => {
-
   return (
     // Should change the value 1 after bug fix for the key in the DB change!
-    <ChakraLink as={ReactRouterLink} to={`bookinfo/${1}`} className="readingCard__link">
-      <Grid
+    <ChakraLink
+      as={ReactRouterLink}
+      to={`bookinfo/${1}`}
+      className="readingCard__link"
+    >
+      <Box
+        className="readingCard__card"
         h="250px"
         p={3}
         borderRadius={10}
-        templateRows="repeat(2, 1fr)"
-        templateColumns="repeat(4, 1fr)"
-        gap={3}
         width={{ base: "100%", md: "100%" }}
         my={3}
         backgroundColor={"gray.200"}
         _hover={{ bg: "gray.300" }}
+        display="flex"
+        flexDirection="column"
+        justifyContent={"space-between"}
       >
-        <GridItem rowSpan={2} colSpan={1}>
-          <Box>
+        <Box className="readingCard__topSection" display="flex">
+          <Box className="readingCard__cover">
             <Image
               boxSize="150px"
               objectFit="contain"
@@ -42,28 +46,30 @@ const ReadingCard: React.FC<ReadingCardProps> = ({ book }): JSX.Element => {
               alt="Placeholder book cover"
             />
           </Box>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Flex>
-            <h1>Title: {"The lord of the rings"}</h1>
-            <Spacer />
-            <p>{"July 23"}</p>
-          </Flex>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <h2>Author: {"J.R.R. Tolkien"}</h2>
-          <h2>Year: {"1954"}</h2>
-          <h2>Pages: {"1178"}</h2>
-        </GridItem>
-        <GridItem colSpan={4}>
+          <Grid templateColumns="repeat(2, 1fr)" width="100%">
+            <GridItem className="readingCard__info">
+              <Heading size="md">{"The lord of the rings"}</Heading>
+              <h2>Author: {"J.R.R. Tolkien"}</h2>
+              <h2>Year: {"1954"}</h2>
+              <h2>Pages: {"1178"}</h2>
+            </GridItem>
+            <GridItem
+              className="readingCard__date"
+              display="flex"
+              justifyContent="flex-end"
+            >
+              <p>{"July 23"}</p>
+            </GridItem>
+          </Grid>
+        </Box>
+        <Box className="readingCard__bottomSection">
           <Flex justifyContent="space-between">
             <h2>Progress:</h2>
             {"30%"}
           </Flex>
-          {/* to fix later */}
           <Progress value={30} size="md" min={0} max={100} />
-        </GridItem>
-      </Grid>
+        </Box>
+      </Box>
     </ChakraLink>
   );
 };
